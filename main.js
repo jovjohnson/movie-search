13f6b95ccaf5a26d73b8d5c84e00d6b5db6258d0
Jovvi Johnson <jjohnsow@gmail.com>
2016-05-23 11:44:19 -0700

    refactor

--- main.js
+++ main.js
@@ -3,15 +3,11 @@
 $(document).ready(init);
 
 function init() {
-
-
 	$('#getMovie').click(getMovie);
 }
 
-
 function getMovie() {
-	
-	//console.log("hey");
+
 	var $input = $("#enter-movie").val();
 	var $yearInput = $("#")
 	$.ajax('http://www.omdbapi.com/?y=&plot=short&r=json&t=' + $input, {
@@ -21,34 +17,23 @@ function getMovie() {
 			var $p = $('<p>');
 			var $year = $('<p>');
 			var $li = $('<li>');
-			
-			//$p.text(data.Director);
+			$p.text(data.Director);
 			$year.text(data.Year);
 			$p.text(data.Title).fadeIn();
-			//$li.text(data.Plot).fadeIn();
+			$li.text(data.Plot).fadeIn();
 			$img.attr('src', data.Poster).addClass('confined').fadeIn();
 			$link.attr('href', 'http://www.imdb.com/title/' + data.imdbID);
-			
+			$('#idk').html($img);
+			$('#results').html($p);
+			// $('#results').html($link);
+			$('#movie-info').append($li);
 
-			$('#movie').append($img);
-			$('#idk').append($p, $link);
-			//$('#movie-info').append($li);
 			$('#enter-movie').val('');
 			$('#enter-year').val('');
-
-
 			console.log(data);
-
-
 		},
-
 		error: function(error) {
 			console.log(error);
 		}
 	})
-}
-
-// function removeInfo {
-
-
-// }
+};
